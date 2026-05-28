f74f80aa7bfed00861d782f401c0287712f49316
shan-luan <shan-luan@outlook.com>
2026-05-29 00:39:44 +0800

    内联TopBar的方法

--- core/src/main/java/com/lomekwi/cave/ui/topbar/TopBar.java
+++ core/src/main/java/com/lomekwi/cave/ui/topbar/TopBar.java
@@ -34,183 +34,150 @@ public class TopBar extends MenuBar {
         super();
 
         addMenu(new MenuX(i18n("文件"))
-            .withItem(new MenuItem(i18n("新建"), new ChangeListenerX(this::newProject))
-                .setShortcut(CONTROL_LEFT, N))
-            .withItem(new MenuItem(i18n("打开"), new ChangeListenerX(this::openProject))
-                .setShortcut(CONTROL_LEFT, O))
-            .withItem(new MenuItemP(i18n("保存"), new ChangeListenerX(this::saveProject))
-                .setShortcut(CONTROL_LEFT, S))
-            .withItem(new MenuItemP(i18n("另存为"), new ChangeListenerX(this::saveAsProject))
-                .setShortcut(CONTROL_LEFT, SHIFT_LEFT, S))
-            .withItem(new MenuItem(i18n("关闭"), new ChangeListenerX(this::closeApplication))
-                .setShortcut(CONTROL_LEFT, W)));
+            .withItem(new MenuItem(i18n("新建"), new ChangeListenerX(() -> {
+                try {
+                    Vars.appEventBus.post(new ProjectLoadedEvent(Projects.create()));
+                    Root.getInstance().getToastManager().show(i18n("项目已新建"), toastTimeOut);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            })).setShortcut(CONTROL_LEFT, N))
+            .withItem(new MenuItem(i18n("打开"), new ChangeListenerX(() -> {
+                try {
+                    NativeFileChooserConfiguration conf = new NativeFileChooserConfiguration();
+                    conf.title = i18n("选择项目...");
+                    if (Gdx.app.getType() == Application.ApplicationType.Android) {
+                        conf.mimeFilter = "*/*";
+                    }
+                    conf.intent = NativeFileChooserIntent.OPEN;
+                    fileChooser.chooseFile(conf, new NativeFileChooserCallback() {
+                        @Override
+                        public void onFileChosen(FileHandle file) {
+                            try {
+                                Vars.appEventBus.post(new ProjectLoadedEvent(Projects.open(file)));
+                                Root.getInstance().getToastManager().show(i18n("项目已打开"), toastTimeOut);
+                            } catch (IOException | ClassNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        @Override
+                        public void onCancellation() {}
+                        @Override
+                        public void onError(Exception exception) {}
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            })).setShortcut(CONTROL_LEFT, O))
+            .withItem(new MenuItemP(i18n("保存"), new ChangeListenerX(() -> {
+                try {
+                    if (Root.getInstance().getFrontendProject() == null) {
+                        return;
+                    }
+
+                    if (Root.getInstance().getFrontendProject().getSavePath() == null) {
+                        NativeFileChooserConfiguration conf = new NativeFileChooserConfiguration();
+                        conf.title = i18n("选择保存位置...");
+                        conf.mimeFilter = "*/*";
+                        conf.intent = NativeFileChooserIntent.SAVE;
+                        fileChooser.chooseFile(conf, new NativeFileChooserCallback() {
+                            @Override
+                            public void onFileChosen(FileHandle file) {
+                                try {
+                                    if (Root.getInstance().getFrontendProject() != null) {
+                                        Projects.save(Root.getInstance().getFrontendProject(), file);
+                                        Root.getInstance().getToastManager().show(i18n("项目已保存"), toastTimeOut);
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            @Override
+                            public void onCancellation() {}
+                            @Override
+                            public void onError(Exception exception) {}
+                        });
+                    } else {
+                        Projects.save(Root.getInstance().getFrontendProject());
+                        Root.getInstance().getToastManager().show(i18n("项目已保存"), toastTimeOut);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            })).setShortcut(CONTROL_LEFT, S))
+            .withItem(new MenuItemP(i18n("另存为"), new ChangeListenerX(() -> {
+                try {
+                    if (Root.getInstance().getFrontendProject() == null) {
+                        return;
+                    }
+
+                    NativeFileChooserConfiguration conf = new NativeFileChooserConfiguration();
+                    conf.title = i18n("选择保存位置...");
+                    conf.mimeFilter = "*/*";
+                    conf.intent = NativeFileChooserIntent.SAVE;
+                    fileChooser.chooseFile(conf, new NativeFileChooserCallback() {
+                        @Override
+                        public void onFileChosen(FileHandle file) {
+                            try {
+                                if (Root.getInstance().getFrontendProject() != null) {
+                                    Projects.save(Root.getInstance().getFrontendProject(), file);
+                                    Root.getInstance().getToastManager().show(i18n("项目已保存"), toastTimeOut);
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        @Override
+                        public void onCancellation() {}
+                        @Override
+                        public void onError(Exception exception) {}
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            })).setShortcut(CONTROL_LEFT, SHIFT_LEFT, S))
+            .withItem(new MenuItem(i18n("关闭"), new ChangeListenerX(() -> {
+                try {
+                    Gdx.app.exit();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            })).setShortcut(CONTROL_LEFT, W)));
 
         addMenu(new MenuX(i18n("工具"))
-            .withItem(new MenuItem(i18n("设置"), new ChangeListenerX(()->{
+            .withItem(new MenuItem(i18n("设置"), new ChangeListenerX(() -> {
                 Vars.appEventBus.post(SettingsOpenedEvent.INSTANCE);
             })))
         );
     }
-    public static class MenuX extends Menu{
+
+    public static class MenuX extends Menu {
         public MenuX(String title) {
             super(title);
         }
-        public MenuX withItem(MenuItem item){
+        public MenuX withItem(MenuItem item) {
             super.addItem(item);
             return this;
         }
-    }
-    /**
-     * 新建项目
-     */
-    private Boolean newProject() {
-        try {
-            Vars.appEventBus.post(new ProjectLoadedEvent(Projects.create()));
-            Root.getInstance().getToastManager().show(i18n("项目已新建"), toastTimeOut);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    /**
-     * 打开项目
-     */
-    private Boolean openProject() {
-        try {
-            NativeFileChooserConfiguration conf = new NativeFileChooserConfiguration();
-            conf.title = i18n("选择项目...");
-            if (Gdx.app.getType() == Application.ApplicationType.Android) {
-                conf.mimeFilter = "*/*";
-            }
-            conf.intent = NativeFileChooserIntent.OPEN;
-            fileChooser.chooseFile(conf, new NativeFileChooserCallback() {
-                @Override
-                public void onFileChosen(FileHandle file) {
-                    try {
-                        Vars.appEventBus.post(new ProjectLoadedEvent(Projects.open(file)));
-                        Root.getInstance().getToastManager().show(i18n("项目已打开"), toastTimeOut);
-                    } catch (IOException | ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-                @Override
-                public void onCancellation() {}
-                @Override
-                public void onError(Exception exception) {}
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    /**
-     * 保存项目
-     */
-    private Boolean saveProject() {
-        try {
-            if (Root.getInstance().getFrontendProject() == null) {
-                return true;
-            }
-
-            if (Root.getInstance().getFrontendProject().getSavePath() == null) {
-                NativeFileChooserConfiguration conf = new NativeFileChooserConfiguration();
-                conf.title = i18n("选择保存位置...");
-                conf.mimeFilter = "*/*";
-                conf.intent = NativeFileChooserIntent.SAVE;
-                fileChooser.chooseFile(conf, new NativeFileChooserCallback() {
-                    @Override
-                    public void onFileChosen(FileHandle file) {
-                        try {
-                            if (Root.getInstance().getFrontendProject() != null) {
-                                Projects.save(Root.getInstance().getFrontendProject(), file);
-                                Root.getInstance().getToastManager().show(i18n("项目已保存"), toastTimeOut);
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    @Override
-                    public void onCancellation() {}
-                    @Override
-                    public void onError(Exception exception) {}
-                });
-            } else {
-                Projects.save(Root.getInstance().getFrontendProject());
-                Root.getInstance().getToastManager().show(i18n("项目已保存"), toastTimeOut);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    /**
-     * 另存为项目
-     */
-    private Boolean saveAsProject() {
-        try {
-            if (Root.getInstance().getFrontendProject() == null) {
-                return true;
-            }
-
-            NativeFileChooserConfiguration conf = new NativeFileChooserConfiguration();
-            conf.title = i18n("选择保存位置...");
-            conf.mimeFilter = "*/*";
-            conf.intent = NativeFileChooserIntent.SAVE;
-            fileChooser.chooseFile(conf, new NativeFileChooserCallback() {
-                @Override
-                public void onFileChosen(FileHandle file) {
-                    try {
-                        if (Root.getInstance().getFrontendProject() != null) {
-                            Projects.save(Root.getInstance().getFrontendProject(), file);
-                            Root.getInstance().getToastManager().show(i18n("项目已保存"), toastTimeOut);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                @Override
-                public void onCancellation() {}
-                @Override
-                public void onError(Exception exception) {}
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    /**
-     * 关闭应用
-     */
-    private Boolean closeApplication() {
-        try {
-            Gdx.app.exit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return true;
     }
 
     /**
      * 与项目关联的菜单项，当当前没有可用项目时会自动禁用
      */
-    public static class MenuItemP extends MenuItem{
+    public static class MenuItemP extends MenuItem {
         public MenuItemP(String text) {
             super(text);
             Vars.appEventBus.register(this);
             setDisabled(true);
         }
-        public MenuItemP(String text, ChangeListener changeListener){
-            super(text,changeListener);
+        public MenuItemP(String text, ChangeListener changeListener) {
+            super(text, changeListener);
             Vars.appEventBus.register(this);
             setDisabled(true);
         }
         @Subscribe
-        public void onTabSwitched(TabSwitchedEvent event){
-            setDisabled(Root.getInstance().getFrontendProject()==null);
+        public void onTabSwitched(TabSwitchedEvent event) {
+            setDisabled(Root.getInstance().getFrontendProject() == null);
         }
     }
 }
