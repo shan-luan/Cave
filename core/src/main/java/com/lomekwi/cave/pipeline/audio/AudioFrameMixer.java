1ca8e5dd35b47116c0b3b61024465cba32a38db0
shan-luan <shan-luan@outlook.com>
2026-06-14 14:02:24 +0800

    一些重命名

--- core/src/main/java/com/lomekwi/cave/pipeline/audio/AudioFrameMixer.java
+++ core/src/main/java/com/lomekwi/cave/pipeline/audio/AudioFrameMixer.java
@@ -3,10 +3,10 @@ package com.lomekwi.cave.pipeline.audio;
 import com.google.common.eventbus.Subscribe;
 import com.lomekwi.cave.app.AppAudioOut;
 
-public class AudioFrameListener {
+public class AudioFrameMixer {
 
     @Subscribe
-    public void onAudFrame(AudFrame frame) {
+    public void sink(AudFrame frame) {
         AppAudioOut.getInstance().getAudioDevice().writeSamples(frame.getSamples(), 0, frame.getSamples().length);
     }
 }
