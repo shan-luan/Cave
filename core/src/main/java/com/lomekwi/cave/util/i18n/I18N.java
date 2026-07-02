40db04ae712dbaa39481311a7aee48dbe7f6cbac
shan-luan <shan-luan@outlook.com>
2026-07-02 08:45:36 +0800

    废弃现有的i18n实现

--- core/src/main/java/com/lomekwi/cave/util/i18n/I18N.java
+++ core/src/main/java/com/lomekwi/cave/util/i18n/I18N.java
@@ -1,10 +1,13 @@
 package com.lomekwi.cave.util.i18n;
 
+@Deprecated
 public class I18N {
     //TODO
+    @Deprecated
     public static String i18n(String key) {
         return key;
     }
+    @Deprecated
     public static String[] i18n(String... keys) {
         for(int i=0; i<keys.length; i++){
             keys[i] = i18n(keys[i]);
