e16f3a552cec5b518b98ab85b121f5825e38101d
shan-luan <shan-luan@outlook.com>
2026-07-15 02:02:33 +0800

    去掉转义序列

--- core/src/main/java/com/lomekwi/cave/ui/editpanel/detail/ImgSrcActor.java
+++ core/src/main/java/com/lomekwi/cave/ui/editpanel/detail/ImgSrcActor.java
@@ -8,8 +8,7 @@ import com.lomekwi.cave.pipeline.image.ImgSrc;
 public class ImgSrcActor extends SourceActor {
     public ImgSrcActor(ImgSrc src) {
         super(src.getDisplayName());
-        add(new VisLabel(i18n("\u8def\u5f84: ") + src.getImgRes().getPath())).pad(4).row();
-        add(new VisLabel(i18n("\u5206\u8fa8\u7387: ") + src.getImgRes().getWidth() + " \u00d7 " + src.getImgRes().getHeight())).pad(4).row();
-        add(new VisLabel(i18n("\u603b\u65f6\u957f: ") + src.getDuration() / 1_000_000.0 + "s")).pad(4);
+        add(new VisLabel(i18n("路径: ") + src.getImgRes().getPath())).pad(4).row();
+        add(new VisLabel(i18n("分辨率: ") + src.getImgRes().getWidth() + " × " + src.getImgRes().getHeight())).pad(4).row();
     }
 }
