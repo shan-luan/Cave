9812300336773c86ab1fa5d1785c8d12a94d30eb
shan-luan <shan-luan@outlook.com>
2026-08-01 00:19:48 +0800

    按enter获取下一个输入框

--- core/src/main/java/com/lomekwi/cave/ui/editpanel/detail/SegDetailView.java
+++ core/src/main/java/com/lomekwi/cave/ui/editpanel/detail/SegDetailView.java
@@ -2,6 +2,16 @@ package com.lomekwi.cave.ui.editpanel.detail;
 
 import static com.lomekwi.cave.util.i18n.I18N.i18n;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.Widget;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.google.common.eventbus.Subscribe;
 import com.lomekwi.cave.app.App;
@@ -11,6 +21,7 @@ import com.kotcrab.vis.ui.widget.VisLabel;
 import com.kotcrab.vis.ui.widget.VisScrollPane;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextButton;
+import com.kotcrab.vis.ui.widget.VisTextField;
 import com.lomekwi.cave.pipeline.Filter;
 import com.lomekwi.cave.pipeline.FilterRegistry;
 import com.lomekwi.cave.pipeline.Source;
@@ -27,6 +38,28 @@ public class SegDetailView extends VisTable {
     private final VisTable content;
     private Segment currentSeg;
     private SegmentSet currentSegSet;
+
+    /**
+     * 回车完成当前输入并把控制权移交给下一个输入框，完全等同于 Tab/Shift+Tab。
+     * 直接委托 VisTextField.next()（与 VisUI 内部 Tab 相同的全舞台空间搜索 + 环绕）。
+     */
+    private final InputListener enterNav = new InputListener() {
+        @Override
+        public boolean keyTyped(InputEvent event, char character) {
+            if (character != '\n' && character != '\r') return false;
+            boolean back = Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)
+                || Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT);
+            Actor self = event.getListenerActor();
+            if (self instanceof VisTextField vis) {
+                vis.next(back);
+            } else if (self instanceof TextField tf) {
+                tf.next(back);
+            } else {
+                return false;
+            }
+            return true;
+        }
+    };
 
     public SegDetailView() {
         content = new VisTable();
@@ -84,6 +117,7 @@ public class SegDetailView extends VisTable {
             first = false;
             appendSegmentInfo(seg);
         }
+        collectInto(content);
     }
 
     private void showInfo(Segment seg) {
@@ -94,6 +128,29 @@ public class SegDetailView extends VisTable {
         content.setFillParent(false);
         content.top();
         appendSegmentInfo(seg);
+        collectInto(content);
+    }
+
+    private void collectInto(Actor actor) {
+        if (actor instanceof TextField || actor instanceof VisTextField) {
+            Widget w = (Widget) actor;
+            if (!w.getListeners().contains(enterNav, true)) {
+                w.addListener(enterNav);
+            }
+            return;
+        }
+        if (actor instanceof Table table) {
+            for (Cell cell : table.getCells()) {
+                Object cellActor = cell.getActor();
+                if (cellActor instanceof Actor act) {
+                    collectInto(act);
+                }
+            }
+        } else if (actor instanceof Group group) {
+            for (Actor child : group.getChildren()) {
+                collectInto(child);
+            }
+        }
     }
 
     private void appendSegmentInfo(Segment seg) {
